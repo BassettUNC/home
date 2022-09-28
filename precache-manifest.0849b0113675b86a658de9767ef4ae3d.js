c490ec721afe00fbd54d1bb13b9a0682b133d0ee
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-09-28 17:24:05 -0400

    Updates

---
+++ precache-manifest.0849b0113675b86a658de9767ef4ae3d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8027c5293479ef3e4aee81b1706f68d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b54653c37bd36e01eb9",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1971ebc0c1bd8cf3c37a",
+    "url": "/static/js/2.72fbaa35.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.72fbaa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b54653c37bd36e01eb9",
+    "url": "/static/js/main.ed8de42d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1f799ce43e0280d2e74df31b81ba6cc0",
+    "url": "/static/media/IMG_1224.1f799ce4.jpg"
+  },
+  {
+    "revision": "414e49a8e94f1495dc1b2b366fc9aa82",
+    "url": "/static/media/e0.414e49a8.png"
+  },
+  {
+    "revision": "0f33fb7b4a55b5c3c45f63a22b8c30ea",
+    "url": "/static/media/e1.0f33fb7b.png"
+  },
+  {
+    "revision": "8338c5989a3d34f12d64118dac95a2b0",
+    "url": "/static/media/e2.8338c598.png"
+  },
+  {
+    "revision": "7c84b9c37598bd8ae09256a3d9549bda",
+    "url": "/static/media/e3.7c84b9c3.png"
+  },
+  {
+    "revision": "a7bf7726612981186d7579f5842fc792",
+    "url": "/static/media/resume.a7bf7726.pdf"
+  }
+]);
