9dad4913d7b9e5a111f23b4abc41e6dbc69cb66a
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-09-28 17:21:20 -0400

    fall22 changes

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -8,7 +8,7 @@ const ExperienceCard = ({data}) => {
     return ( 
         <Col lg="6">
             <div className="pb-5 text-center">
-                <img className=" bg-white mb-3" src={data.companylogo} alt=""/>
+                <img className=" bg-white mb-3" src={data.companylogo} width="359" height="186" style={{border: '3px solid #6c757d',borderRadius: '10px'}} alt=""/>
                 <p className="lead">
                     {data.role}
                     <br/>
